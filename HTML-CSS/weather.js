cf00e0ad1ab8fb55a7e326bf36df3a6af8999530
Turgay Levent <turgayleventt@gmail.com>
2022-09-30 10:41:35 +0300

    versiyon2

---
+++ HTML-CSS/weather.js
@@ -0,0 +1,12 @@
+const form = document.querySelector(`.top-banner form`);
+const input = document.querySelector(`.container input`);
+const msg = document.querySelector(`span.msg`);
+const list = document.querySelector(`.ajax-section ul.cities`);
+localStorage.setItem(
+  `tokenKey`,
+  `Gojkk5iDf3ciB9FxbRq9pPwkyHgu4xChzVyhG4JjIQk7+P1r6L5wZWDSxuTdIHmb`
+);
+// localStorage.setItem(
+//   `tokenKeyEncrypted`,
+//   EncryptStringAES(`d7938bfaa9a0fc0e16466a45a23f82ac`)
+// );
